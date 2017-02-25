e190c2fa8cd17303867c9987e4ae25d0efbfab95
Jamieson Bates <jamiesonbates@gmail.com>
2017-02-25 14:25:51 -0800

    Finish html mockup and begin building JS

---
+++ index_FINISHED.js
@@ -0,0 +1,40 @@
+// Select all keys
+const $keys = $('.key');
+
+$keys.on('click', (event) => {
+  const key = $(event.target).attr('data-key');
+  console.log(key);
+  switch (key) {
+    case 65:
+
+      break;
+
+    case 83:
+
+      break;
+
+    case 68:
+
+      break;
+
+    case 70:
+
+      break;
+
+    case 71:
+
+      break;
+
+    case 72:
+
+      break;
+
+    case 74:
+
+      break;
+
+    case 75:
+
+      break;
+  }
+});
