e16be8f8f60273977c8204f6a278b239e9121d7f
Jamieson Bates <jamiesonbates@gmail.com>
2017-02-26 10:15:16 -0800

    Add transitionend event to remove visual effect

--- index_FINISHED.js
+++ index_FINISHED.js
@@ -1,14 +1,14 @@
-// Select all keys
-const $keys = $('.key');
-
 $(window).on('keydown', (event) => {
   const $key = $(`.key[data-key=${event.keyCode}]`);
-  console.log($key);
   const $audio = $(`audio[data-key=${event.keyCode}]`)[0];
-  console.log($audio);
+
   $key.addClass('playing');
 
   $audio.currentTime = 0;
 
   $audio.play();
+});
+
+$('.key').on('transitionend', (event) => {
+  $(event.target).removeClass('playing');
 });
